4ea8fc0ee44568472f4ccca59493c623314c50b5
laun4er1993 <laun4er1993@gmail.com>
2026-03-24 00:04:30 +0300

    Create states.py

---
+++ states/states.py
@@ -0,0 +1,9 @@
+# states/states.py
+from aiogram.fsm.state import State, StatesGroup
+
+class SearchStates(StatesGroup):
+    waiting_for_village = State()
+    waiting_for_kml = State()
+    waiting_for_txt_upload = State()
+    waiting_for_district_select = State()
+    waiting_for_add_village = State()
