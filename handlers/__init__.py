9eab7a916fc7ba7ca09b6649435f8dc7fadb70fa
laun4er1993 <laun4er1993@gmail.com>
2026-03-24 00:03:50 +0300

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,14 @@
+# handlers/__init__.py
+from handlers.start import register_start_handlers
+from handlers.search import register_search_handlers
+from handlers.kml import register_kml_handlers
+from handlers.settings import register_settings_handlers
+from handlers.callbacks import register_callbacks
+
+__all__ = [
+    'register_start_handlers',
+    'register_search_handlers',
+    'register_kml_handlers',
+    'register_settings_handlers',
+    'register_callbacks'
+]
