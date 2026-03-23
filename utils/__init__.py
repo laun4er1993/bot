010a084ad0ba286ddd8898b7585a9d7625899ce9
laun4er1993 <laun4er1993@gmail.com>
2026-03-24 00:04:32 +0300

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+# utils/__init__.py
+from utils.helpers import safe_edit_text, safe_answer_callback, safe_delete_message
+
+__all__ = [
+    'safe_edit_text',
+    'safe_answer_callback',
+    'safe_delete_message'
+]
