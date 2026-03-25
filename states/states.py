25d7147271412273af9edb8c5d7730a5d2d41dea
laun4er1993 <laun4er1993@gmail.com>
2026-03-25 03:35:25 +0300

    Update states.py

--- states/states.py
+++ states/states.py
@@ -7,7 +7,7 @@ class SearchStates(StatesGroup):
     waiting_for_txt_upload = State()
     waiting_for_district_select = State()
     waiting_for_add_village = State()
-    waiting_for_afs_upload = State()  # Состояние для загрузки каталога АФС
-    waiting_for_add_kml = State()     # Состояние для добавления KML вручную
-    waiting_for_kml_upload = State()  # Состояние для загрузки каталога KML
+    waiting_for_afs_upload = State()
+    waiting_for_add_kml = State()
+    waiting_for_kml_upload = State()  # Для загрузки KML файлов
     downloading_in_progress = State()
