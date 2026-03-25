ce54e35523193adb5bbd8e3ec3ecaf453335cb9f
laun4er1993 <laun4er1993@gmail.com>
2026-03-25 03:35:20 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -3,10 +3,14 @@ from services.yandex_disk import YandexDiskClient
 from services.village_db import VillageDatabase
 from services.photos_db import PhotosDatabase
 from services.kml_processor import KMLProcessor
+from services.afs_catalog import AFSCatalog
+from services.kml_catalog import KMLCatalog
 
 __all__ = [
     'YandexDiskClient',
     'VillageDatabase',
     'PhotosDatabase',
-    'KMLProcessor'
+    'KMLProcessor',
+    'AFSCatalog',
+    'KMLCatalog'
 ]
