54ba1b6fd9e9808c7a8ccc853261365a916d44ee
laun4er1993 <laun4er1993@gmail.com>
2026-03-27 16:55:13 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,11 +3,13 @@ from handlers.search import register_search_handlers
 from handlers.kml import register_kml_handlers
 from handlers.settings import register_settings_handlers
 from handlers.callbacks import register_callbacks
+from handlers.coord_calculator import register_coord_calculator_handlers
 
 __all__ = [
     'register_start_handlers',
     'register_search_handlers',
     'register_kml_handlers',
     'register_settings_handlers',
-    'register_callbacks'
+    'register_callbacks',
+    'register_coord_calculator_handlers'
 ]
